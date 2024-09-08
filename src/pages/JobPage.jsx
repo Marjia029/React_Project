70537508e9495532c442bd82b4fb1171f45b42d5
Marjia <marjiaafroj2@gmail.com>
2024-09-09 02:40:49 +0600

    used dataLoader to fetch data

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
+//import {useState, useEffect} from 'react'
+import { useParams, useLoaderData } from 'react-router-dom';
 
-import Spinners from '../components/Spinners';
+//import Spinners from '../components/Spinners';
 
 
 
 const JobPage = () => {
     const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const job = useLoaderData();
+    // const [job, setJob] = useState(null);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data);
-              }catch(error) {
-                console.error('Error fetching job', error);
-              }finally {
-                setLoading(false);
-              }
-        }
-        fetchJob();
-    }, []);    
-  return loading ? <Spinners /> : (<h1>{job.title}</h1>)
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`);
+    //             const data = await res.json();
+    //             setJob(data);
+    //           }catch(error) {
+    //             console.error('Error fetching job', error);
+    //           }finally {
+    //             setLoading(false);
+    //           }
+    //     }
+    //     fetchJob();
+    // }, []);    
+  return <h1>{job.title}</h1>
   
 }
 
-export default JobPage
+const jobLoader = async({params}) =>{
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+
+};
+
+export {JobPage as default, jobLoader}
