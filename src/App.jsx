f80590d04fa25201518cb00c5c7ce0170d92309b
Marjia <marjiaafroj2@gmail.com>
2024-09-09 12:32:10 +0600

    Creating Form in add job page to add job

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 
 
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route index element = {<HomePage/>}/>
       <Route path='/jobs' element = {<JobsPage/>}/>
       <Route path='/jobs/:id' element = {<JobPage/>} loader = {jobLoader}/>
+      <Route path='/add-job' element = {<AddJobPage/>}/>
       <Route path='*' element = {<NotFoundPage/>}/>
     </Route>
   )
